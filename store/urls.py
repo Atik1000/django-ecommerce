132d0c5daefffa4c6bcdc993a0b4b6e52257fcd6
Atik1000 <foysalkazi45@gmail.com>
2020-12-27 15:25:48 +0600

    templating added complete

--- store/urls.py
+++ store/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    # Leave as empty string for base url
     path('', store, name="store"),
     path('cart/', cart, name="cart"),
     path('checkout/', checkout, name="checkout"),
